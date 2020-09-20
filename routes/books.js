5b37f386f8cd880b2c7e274ffaa2edbc18b1f8ce
Jennifer <jennifer.nordell@outlook.com>
2020-09-19 21:59:08 -0500

    delete works form errors abstracted out

--- routes/books.js
+++ routes/books.js
@@ -21,30 +21,79 @@ router.get('/', asyncHandler(async (req, res) => {
 
 
 /* Create a new book form. */
-router.get('/books/new', (req, res) => {
+router.get('/new', (req, res) => {
   res.render("books/new", { article: {}, title: "New Book" });
 });
 
 /* POST create book. */
-router.post('/books/new', asyncHandler(async (req, res) => {
-  const article = await Book.create(req.body);
-  res.redirect("/books/" + book.id);
+router.post('/new', asyncHandler(async (req, res) => {
+  let book;
+  try {
+    book = await Book.create(req.body);
+    res.redirect("/books");
+  } catch(error) {
+    if(error.name === "SequelizeValidationError") {
+      book = await Book.build(req.body);
+      res.render("books/new", {book, errors: error.errors, title: "New Book" })
+    } else {
+      throw error;
+    }
+  }
 }));
 
 
 /* GET individual book form */
-router.get("/books/:id", asyncHandler(async (req, res) => {
-  res.render("books/edit", { article: {}, title: "Book Details" }); 
+router.get("/:id", asyncHandler(async (req, res) => {
+  console.log(req.params.id);
+  const book = await Book.findByPk(req.params.id);
+  if(book) {
+    res.render("books/edit", { book,  title: "Update Book" });
+  } else {
+    res.sendStatus(404);
+  } 
 }));
 
 /* Update a book. */
-router.post('/books/:id', asyncHandler(async (req, res) => {
-  res.redirect("/books/");
+router.post('/:id/edit', asyncHandler(async (req, res) => {
+  let book;
+  try {
+    book = await Book.findByPk(req.params.id);
+    if(book) {
+      await book.update(req.body);
+      res.redirect("/books");
+    } else {
+      res.sendStatus(404);
+    }
+  } catch(error) {
+    if(error.name === "SequelizeValidationError") {
+      book = await Book.build(req.body);
+      book.id = req.params.id;
+      res.render("books/edit", { book, errors: error.errors, title: "Update Book" })
+    } else {
+      throw error;
+    }
+  }
 }));
 
 /* Delete book. */
-router.get("/books/:id/delete", asyncHandler(async (req, res) => {
-  res.render("books/delete", { article: {}, title: "Delete Book" });
+router.get("/:id/delete", asyncHandler(async (req, res) => {
+  const book = await Book.findByPk(req.params.id);
+  if(book) {
+    res.redirect("/books");
+  } else {
+    res.sendStatus(404);
+  }
+}));
+
+/* Delete book. */
+router.post("/:id/delete", asyncHandler(async (req, res) => {
+  const book = await Book.findByPk(req.params.id);
+  if(book) {
+    await book.destroy();
+    res.redirect("/books");
+  } else {
+    res.sendStatus(404);
+  }
 }));
 
 module.exports = router;
