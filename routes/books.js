7007d48abaeb49fa7a4545db4e1c62ab649914b9
Jennifer <jennifer.nordell@outlook.com>
2020-09-24 10:22:02 -0500

    pagination works for all books

--- routes/books.js
+++ routes/books.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Book = require('../models').Book;
 const { Op } = require("sequelize");
+const perPage = 10;
 
 /* Handler function to wrap each route. */
 function asyncHandler(cb){
@@ -14,10 +15,42 @@ function asyncHandler(cb){
   }
 }
 
+/* Function to return a list of numbers used for the pagination buttons */
+function getButtonList(active, total) {
+  let buttons;
+  let current = parseInt(active);
+  let all = parseInt(total);
+  if(current <= 3) {
+    buttons = [1, 2, 3, 4, 5];
+  } else if( current >= all - 3) {
+    buttons = [all - 4, all - 3, all - 2 ,all -1, all]
+  } else {
+    buttons = [current - 2, current - 1, current, current + 1, current + 2]
+  }
+  return buttons;
+}
+
 /* Books listing for all books */
 router.get('/', asyncHandler(async (req, res) => {
-  const books = await Book.findAll({ order: [["title"]]});
-  res.render("books/index", { books, title: "All Books"});
+  let page;
+  if(!req.query.p){
+    page = 1;
+  } else {
+    page = req.query.p;
+  }
+
+  const { count, rows } = await Book.findAndCountAll({ 
+    order: [["title"]],
+    limit: perPage,
+    offset: (page - 1) * perPage
+  });
+  
+  console.log(page);
+  // console.log(rows);
+  let books = rows;
+  let numOfPages = Math.ceil(count / perPage);
+  let buttons = getButtonList(page, numOfPages);
+  res.render("books/index", { books, numOfPages, buttons, page, title: "All Books"});
 }))
 
 
